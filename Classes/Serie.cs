6dfd859e422695f609882f2382ef4d6f75c8a207
goesrafa <62679926+goesrafa@users.noreply.github.com>
2021-09-08 12:37:28 -0300

    Add files via upload

---
+++ Classes/Serie.cs
@@ -0,0 +1,53 @@
+namespace DIO.Series
+{
+    public class Serie : EntidadeBase
+    {
+        //Atributos
+
+        private Genero Genero {get; set}
+
+        private string Titulo {get; set}
+
+        private string Descricao {get; set}
+
+        private int Ano {get; set}
+
+        private bool Excluido{get; set}
+
+        //Metodos
+
+        public Series (int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+         
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Envaironment.NewLine;
+            retorno += "Título: " + this.Titulo + Envaironment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Envaironment.NewLine;
+            retorno += "Ano de Inicio: " + this.Ano;
+            return retorno;
+        }
+
+        public string retornaTitulo()
+        {
+            return this.Titulo;
+        }
+        public int retornaId()
+        {
+            return this.Id;
+        }
+        public void Excluir(){
+            this.Excluido = true;
+        }
+
+    }
+}
